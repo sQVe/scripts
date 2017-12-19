9458bb8e16bca32490f91f558ed87f9c7ac3c4fa
Oskar Grunning <oskargrunning@gmail.com>
2017-12-19 23:01:17 +0100

    Add kamajii mount script

---
+++ mount-kamajii.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' 
+
+sudo mkdir -p /mnt/kamajii
+sudo mkdir -p /mnt/kamajii/backup
+sudo mkdir -p /mnt/kamajii/complete
+sudo mkdir -p /mnt/kamajii/incomplete
+sudo mkdir -p /mnt/kamajii/nzb
+
+
+echo -e "${CYAN}Mounting kamajii...${NC}"
+
+echo -n 'backup: '
+sudo mount -t nfs -o vers=4 kamajii:/srv/nfs/backup /mnt/kamajii/backup
+echo -e "${GREEN}OK${NC}"
+
+echo -n 'complete: '
+sudo mount -t nfs -o vers=4 kamajii:/srv/nfs/complete /mnt/kamajii/complete
+echo -e "${GREEN}OK${NC}"
+
+echo -n 'incomplete: '
+sudo mount -t nfs -o vers=4 kamajii:/srv/nfs/incomplete /mnt/kamajii/incomplete 
+echo -e "${GREEN}OK${NC}"
+
+echo -n 'nzb: '
+sudo mount -t nfs -o vers=4 kamajii:/srv/nfs/nzb /mnt/kamajii/nzb
+echo -e "${GREEN}OK${NC}"
