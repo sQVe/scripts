01a93ad724119f66a243a6febd05db64ff60525f
Oskar Grunning <oskargrunning@gmail.com>
2017-12-22 13:57:30 +0100

    Add portable hdd share

--- mount-shishigami.sh
+++ mount-shishigami.sh
@@ -8,6 +8,7 @@ sudo mkdir -p /mnt/shishigami
 sudo mkdir -p /mnt/shishigami/media
 sudo mkdir -p /mnt/shishigami/misc
 sudo mkdir -p /mnt/shishigami/pictures
+sudo mkdir -p /mnt/shishigami/portable-hdd
 
 
 echo -e "${CYAN}Mounting shishigami...${NC}"
@@ -22,4 +23,8 @@ echo -e "${GREEN}OK${NC}"
 
 echo -n 'pictures: '
 sudo mount -t nfs -o vers=4 shishigami:/volume1/pictures /mnt/shishigami/pictures
+echo -e "${GREEN}OK${NC}"
+
+echo -n 'portable-hdd: '
+sudo mount -t nfs -o vers=4 shishigami:/volumeUSB1/usbshare /mnt/shishigami/portable-hdd
 echo -e "${GREEN}OK${NC}"
