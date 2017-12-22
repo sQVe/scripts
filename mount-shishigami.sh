0b9da29fa1277f81431d6540df591e82302c14c5
Oskar Grunning <oskargrunning@gmail.com>
2017-12-22 09:41:41 +0100

    Move shares

--- mount-shishigami.sh
+++ mount-shishigami.sh
@@ -5,26 +5,21 @@ CYAN='\033[0;36m'
 NC='\033[0m' 
 
 sudo mkdir -p /mnt/shishigami
+sudo mkdir -p /mnt/shishigami/media
 sudo mkdir -p /mnt/shishigami/misc
-sudo mkdir -p /mnt/shishigami/movies
 sudo mkdir -p /mnt/shishigami/pictures
-sudo mkdir -p /mnt/shishigami/series
 
 
 echo -e "${CYAN}Mounting shishigami...${NC}"
+
+echo -n 'media: '
+sudo mount -t nfs -o vers=4 shishigami:/volume1/media /mnt/shishigami/media 
+echo -e "${GREEN}OK${NC}"
 
 echo -n 'misc: '
 sudo mount -t nfs -o vers=4 shishigami:/volume1/misc /mnt/shishigami/misc
 echo -e "${GREEN}OK${NC}"
 
-echo -n 'movies: '
-sudo mount -t nfs -o vers=4 shishigami:/volume1/movies /mnt/shishigami/movies 
-echo -e "${GREEN}OK${NC}"
-
 echo -n 'pictures: '
 sudo mount -t nfs -o vers=4 shishigami:/volume1/pictures /mnt/shishigami/pictures
-echo -e "${GREEN}OK${NC}"
-
-echo -n 'series: '
-sudo mount -t nfs -o vers=4 shishigami:/volume1/series /mnt/shishigami/series
 echo -e "${GREEN}OK${NC}"
