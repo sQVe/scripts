de7c0b80b7da7cc52cf52637022549da75399b8d
Oskar Grunning <oskargrunning@gmail.com>
2018-03-27 11:17:38 +0200

    Add new nvm-handler script

--- nvm-update-node.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source /usr/share/nvm/nvm.sh --no-use
-
-current_version=$(nvm current)
-nvm install node --reinstall-packages-from="$current_version"
-nvm uninstall "$current_version"
