1b479de779aebb4592d54a13618f9e513f59ef17
Oskar Grunning <oskargrunning@gmail.com>
2018-09-06 09:52:01 +0200

    Remove random wall script This was moved to the bin repo

--- random-wall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#  ┏━┓┏━┓┏┓╻╺┳┓┏━┓┏┳┓   ╻ ╻┏━┓╻  ╻
-#  ┣┳┛┣━┫┃┗┫ ┃┃┃ ┃┃┃┃   ┃╻┃┣━┫┃  ┃
-#  ╹┗╸╹ ╹╹ ╹╺┻┛┗━┛╹ ╹   ┗┻┛╹ ╹┗━╸┗━╸
-
-feh --randomize --bg-fill ~sqve/pictures/wallpapers/*
