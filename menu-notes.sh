8a1b431774c2a5602f6262b6b2e45921dd660b1c
Oskar Grunning <oskargrunning@gmail.com>
2019-01-21 10:49:11 +0100

    Repair notes menu

--- menu-notes.sh
+++ menu-notes.sh
@@ -4,10 +4,10 @@
 #  ┃┃┃┣╸ ┃┗┫┃ ┃   ┃┗┫┃ ┃ ┃ ┣╸ ┗━┓
 #  ╹ ╹┗━╸╹ ╹┗━┛   ╹ ╹┗━┛ ╹ ┗━╸┗━┛
 
-notes="$(fd --type f . "$HOME/notes" | sed -r 's/^.+notes\///')"
+notes="$(command fd --extension md . "$HOME/notes" | sed -r -e 's/\.md//' -e 's/^.+notes\///')"
 choice="$(echo "$notes" | rofi -kb-accept-entry "Return" -dmenu -p 'run')"
 
 
-if [[ -n "$choice" && -f "$HOME/notes/$choice" ]]; then
-  term nvim "$HOME/notes/$choice"
+if [[ -n "$choice" && -f "$HOME/notes/$choice.md" ]]; then
+  term nvim "$HOME/notes/$choice.md"
 fi
