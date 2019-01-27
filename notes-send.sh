e3c69fa9ce40d16a5412b096b13fc6696d7091a9
Oskar Grunning <oskargrunning@gmail.com>
2019-01-27 20:42:56 +0100

    Update and repair note-send script

--- notes-send.sh
+++ notes-send.sh
@@ -6,37 +6,32 @@
 
 message="Automatic update ($(hostname))"
 
-identity_count="$(ssh-add -l | wc -l)"
-status_count="$(git status --short | wc -l)"
+identity_count=$(ssh-add -l | wc -l)
+status_count=$(git status --short | wc -l)
+unpushed_commit_count="$(git diff --numstat origin/master..HEAD | awk '{print $1}')"
 
-get_system_date() {
-  timedatectl | ag 'Universal time' | awk '{print $4}'
-}
-
-get_latest_commit_date() {
-  git log -1 --format=%ci | awk '{print $1}'
-}
-
-get_latest_commit_status() {
-  git log -1 --format=%s
-}
+latest_commit_date="$(git log -1 --format=%ci | awk '{print $1}')"
+latest_commit_status="$(git log -1 --format=%s)"
+system_date="$(timedatectl | ag 'Universal time' | awk '{print $4}')"
 
 if [[ identity_count -eq 0 ]]; then
   echo "No SSH identities found. Exiting."
   exit
-elif [[ status_count -eq 0 ]]; then
-  echo "No file changes. Exiting."
+elif [[ status_count -eq 0 ]] && [[ unpushed_commit_count -eq 0 ]]; then
+  echo "No file changes or unpushed commits. Exiting."
   exit
 fi
 
-
-if [[ "$(get_latest_commit_status)" != "$message" ]] ||
-   [[ "$(get_latest_commit_date)" != "$(get_system_date)" ]]; then
+if [[ "$latest_commit_status" != "$message" ]] ||
+   [[ "$latest_commit_date" != "$system_date" ]]; then
+  # New daily commit.
   git commit --all --message "$message"
   git push
 else
+  # Amend to existing daily commit.
   git commit --all --amend --reuse-message HEAD
   git push --force
 fi
 
-git fetch
+git pull --rebase
+
