fa7134cad102c98852ab7f5e70e6a62d781d257f
Oskar Grunning <oskargrunning@gmail.com>
2019-02-22 09:13:49 +0100

    Extend brightness toggle script

--- toggle-brightness.sh
+++ toggle-brightness.sh
@@ -6,8 +6,10 @@
 
 current_brightness=$(light | cut -d '.' -f 1)
 
-if [[ current_brightness -lt 50 ]]; then
+if [[ current_brightness -lt 20 ]]; then
+  light -S 40 && pkill -RTMIN+10 i3blocks
+elif [[ current_brightness -lt 60 ]]; then
   light -S 80 && pkill -RTMIN+10 i3blocks
 else
-  light -S 5 && pkill -RTMIN+10 i3blocks
+  light -S 10 && pkill -RTMIN+10 i3blocks
 fi
