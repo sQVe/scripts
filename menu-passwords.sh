7767376f0b3eaf0222faf6ac6332a95b91304ab7
Oskar Grunning <oskargrunning@gmail.com>
2019-03-09 11:16:47 +0100

    Only show password notification for 1 second

--- menu-passwords.sh
+++ menu-passwords.sh
@@ -8,5 +8,5 @@ passwords=$(fd --extension gpg . "$PASSWORD_STORE_DIR" | sed "s#$PASSWORD_STORE_
 choice="$(echo "$passwords" | rofi -kb-accept-entry "Return" -dmenu -p 'run')"
 
 if [[ -n "$choice" ]]; then
-  pass show --clip "$choice" && notify-send -i password "Password copied" "$choice"
+  pass show --clip "$choice" && notify-send -t 1000 -i password "Password copied" "$choice"
 fi
