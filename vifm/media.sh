38b84befa7b929bc5ac8eabb31427453f8533ea6
Oskar Grunning <oskargrunning@gmail.com>
2019-04-29 23:06:51 +0200

    Add vifm specific scripts

---
+++ vifm/media.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#  ┏┳┓┏━╸╺┳┓╻┏━┓
+#  ┃┃┃┣╸  ┃┃┃┣━┫
+#  ╹ ╹┗━╸╺┻┛╹╹ ╹
+
+list() {
+  mapfile -t devices < <(
+    udiskie-info --all --output \
+      "toplevel={is_toplevel};device={device_file};label={id_label};mount-point={mount_path}" | \
+      rg '^toplevel=False;' | sed 's/^toplevel=False;//' | sed 's/;mount-point=$//'
+  )
+
+  for device in "${devices[@]}"; do
+    sed 's/;/\n/g' <<< "$device"
+  done
+}
+
+help() {
+  echo "Usage: media list | mount <device> | unmount <path>"
+  exit 1
+}
+
+case "$1" in
+  list) list ;;
+  mount) udiskie-mount "$2" ;;
+  unmount) udiskie-umount "$2" ;;
+  *) help ;;
+esac
