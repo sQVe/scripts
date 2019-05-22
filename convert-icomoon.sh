38bfc96e99c9097ce4b1aad03c176d5dd9d69edd
Oskar Grunning <oskargrunning@gmail.com>
2019-05-22 21:22:05 +0200

    Update icomoon convert script

--- convert-icomoon.sh
+++ convert-icomoon.sh
@@ -4,6 +4,9 @@
 #  ┃  ┃ ┃┃┗┫┃┏┛┣╸ ┣┳┛ ┃    ┃┃  ┃ ┃┃┃┃┃ ┃┃ ┃┃┗┫
 #  ┗━╸┗━┛╹ ╹┗┛ ┗━╸╹┗╸ ╹    ╹┗━╸┗━┛╹ ╹┗━┛┗━┛╹ ╹
 
+# Convert the icon definitions, in the generated icomoon `style.css`, to a scss
+# variable list.
+
 if [[ $# -eq 0 ]]; then
   icomoon_styling="$(cat)"
 else
@@ -11,11 +14,15 @@ else
 fi
 
 function clean_icon_definitions() {
-  sed -r 's#^\.icon-(\w+).+#\t\1: #'
+  sed -r 's/^\.icon-([a-z1-9_-]+).+/\t\1: /i'
 }
 
 function clean_content_definitions() {
-  sed -r 's#^\s.+("\\\w+").+#\1,#'
+  sed -r 's/^\s.+("\\\w+").+/\1,/'
+}
+
+function replace_underscores() {
+  sed -r 's/_/-/'
 }
 
 function header() {
@@ -34,14 +41,16 @@ function join_lines() {
   awk '{printf (NR%2 == 0) ? $0 "\n" : $0}'
 }
 
-function convert() {
+function convert_definitions() {
   echo "$icomoon_styling"     |
     find_icon_definitions     |
     clean_icon_definitions    |
     clean_content_definitions |
-    join_lines
+    replace_underscores       |
+    join_lines                |
+    sort -u
 }
 
 echo -n "$(header)
-$(convert)
+$(convert_definitions)
 $(footer)"
