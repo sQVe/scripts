a195fce407996ceb58ace12c3c94af9cb21d2562
Oskar Grunning <oskargrunning@gmail.com>
2019-09-02 10:32:34 +0200

    Disable word splitting warning

--- reinstall-git-packages.sh
+++ reinstall-git-packages.sh
@@ -4,4 +4,7 @@
 #  ┣┳┛┣╸ ┃┃┗┫┗━┓ ┃ ┣━┫┃  ┃     ┃╺┓┃ ┃    ┣━┛┣━┫┃  ┣┻┓┣━┫┃╺┓┣╸ ┗━┓
 #  ╹┗╸┗━╸╹╹ ╹┗━┛ ╹ ╹ ╹┗━╸┗━╸   ┗━┛╹ ╹    ╹  ╹ ╹┗━╸╹ ╹╹ ╹┗━┛┗━╸┗━┛
 
+# shellcheck disable=SC2046
+# Do not warn about word splitting.
+
 yay -S $(yay -Qqs git | rg '\w+\-git$')
