970b0f056c8543c0e8fae1cde0a2b308c9644691
Oskar Grunning <oskargrunning@gmail.com>
2020-04-18 19:45:03 +0200

    Repair default node version script

--- default-node-version.sh
+++ default-node-version.sh
@@ -9,12 +9,12 @@ default_version=$(cat "$NVM_DIR/alias/default")
 available_versions=$(ls -1 "$base_path")
 
 function get_default_version() {
-  rg "$default_version" <<<"$available_versions"
+  rg "^v$default_version" <<<"$available_versions"
 }
 
 function get_latest_version() {
   echo "$available_versions" | sort -V | tail -n 1
 }
 
-version=$(get_default_version || get_latest_version) &&
-  echo "$base_path/$version/bin"
+version=$(get_default_version || get_latest_version)
+echo "$base_path/$version/bin"
