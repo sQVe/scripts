c45b64945de570447c8a899b914f6e2af0538494
Oskar Grunning <oskargrunning@gmail.com>
2020-05-19 15:07:22 +0200

    Move over to z.lua

--- zlua/list-directories.zsh
+++ zlua/list-directories.zsh
@@ -4,6 +4,6 @@
 #  ┃  ┃┗━┓ ┃     ┃┃┃┣┳┛┣╸ ┃   ┃ ┃ ┃┣┳┛┃┣╸ ┗━┓
 #  ┗━╸╹┗━┛ ╹    ╺┻┛╹╹┗╸┗━╸┗━╸ ╹ ┗━┛╹┗╸╹┗━╸┗━┛
 
-source /usr/share/z/z.sh
+eval "$(lua $ZIM_HOME/modules/z.lua/z.lua --init zsh)"
 
 z -l | awk '{print $2}'
