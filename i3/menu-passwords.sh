053f602855f04798ac1e0226c8fcbaf2f7966806
Oskar Grunning <oskargrunning@gmail.com>
2020-10-05 00:57:53 +0200

    Support filtering password down via input

--- i3/menu-passwords.sh
+++ i3/menu-passwords.sh
@@ -5,7 +5,7 @@
 #  ╹ ╹┗━╸╹ ╹┗━┛   ╹  ╹ ╹┗━┛┗━┛┗┻┛┗━┛╹┗╸╺┻┛┗━┛
 
 passwords=$(fd --extension gpg . "$PASSWORD_STORE_DIR" | sed "s#$PASSWORD_STORE_DIR/##" | sed 's/\.gpg$//')
-choice="$(echo "$passwords" | rofi -kb-accept-entry "Return" -dmenu -theme-str 'inputbar { children: [prompt, entry]; }' -p 'password: ')"
+choice="$(echo "$passwords" | rg "$1" | rofi -kb-accept-entry "Return" -dmenu -theme-str 'inputbar { children: [prompt, entry]; }' -p 'password: ')"
 
 if [[ -n "$choice" ]]; then
   if pass show --clip "$choice"; then
