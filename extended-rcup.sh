7a1751ea57d97c3f7ffe301de61d752816693fc4
Oskar Grunning <oskargrunning@gmail.com>
2020-10-20 21:11:28 +0200

    Tweak extended rcup

--- extended-rcup.sh
+++ extended-rcup.sh
@@ -9,9 +9,7 @@
 rcup "$@"
 
 # Only create custom symlinks when script has no arguments.
-if [[ -z "$*" ]]; then
+if [[ $# -eq 0 ]]; then
   ln -s "$DOTFILES/config/zsh/zimrc" "$XDG_CONFIG_HOME/zsh/.zimrc"
   ln -s "$DOTFILES/config/zsh/zshenv" "$XDG_CONFIG_HOME/zsh/.zshenv"
-  ln -s "$DOTFILES/config/zsh/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
-
 fi
