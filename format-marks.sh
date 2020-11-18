c08754b5c79dbd48913d9076f96d9ad00bee7859
Oskar Grunning <oskargrunning@gmail.com>
2020-11-18 20:18:09 +0100

    Tweak format bookmarks script

--- format-marks.sh
+++ format-marks.sh
@@ -7,5 +7,5 @@
 bookmarks="$DOTFILES/config/qutebrowser/bookmarks/urls"
 quickmarks="$DOTFILES/config/qutebrowser/quickmarks"
 
-command nvim "$bookmarks" "+ %sort /\v^.{-}\s+/ u | %EasyAlign \ " "+wq"
+command nvim "$bookmarks" "+ %sort /\v^.{-}\s+/ u" "+ %EasyAlign \ " "+wq"
 command nvim "$quickmarks" "+%sort u | %EasyAlign \ " "+wq"
