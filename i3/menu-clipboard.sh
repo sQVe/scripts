fe7b0395ed1320d6917f4b24e7a2ad3280e96053
Oskar Grunning <oskargrunning@gmail.com>
2021-03-05 08:12:04 +0100

    Cleanup unused scripts

--- i3/menu-clipboard.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#  ┏┳┓┏━╸┏┓╻╻ ╻   ┏━╸╻  ╻┏━┓┏┓ ┏━┓┏━┓┏━┓╺┳┓
-#  ┃┃┃┣╸ ┃┗┫┃ ┃   ┃  ┃  ┃┣━┛┣┻┓┃ ┃┣━┫┣┳┛ ┃┃
-#  ╹ ╹┗━╸╹ ╹┗━┛   ┗━╸┗━╸╹╹  ┗━┛┗━┛╹ ╹╹┗╸╺┻┛
-
-rofi -modi 'clipboard:greenclip print' -run-command '{cmd}' -show clipboard
--- mutt/compose.sh
+++
@@ -1,13 +0,0 @@
-#!/usr/bin/env bash
-
-#  ┏━╸┏━┓┏┳┓┏━┓┏━┓┏━┓┏━╸
-#  ┃  ┃ ┃┃┃┃┣━┛┃ ┃┗━┓┣╸
-#  ┗━╸┗━┛╹ ╹╹  ┗━┛┗━┛┗━╸
-
-is_reply_included=$(rg '^On\s+' | rg 'wrote:$' <"$1" | wc -l)
-
-if [[ $is_reply_included -gt 0 ]]; then
-  nvim "+norm /OnOO" "+StripWhitespace" "$1"
-else
-  nvim "+norm Go" "+StripWhitespace" "$1"
-fi
