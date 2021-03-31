e04f7f8d22667fe02d356b82f85dcacec0c4e0ed
Oskar Grunning <oskargrunning@gmail.com>
2021-03-31 11:53:00 +0200

    Add new redshift toggle script

---
+++ i3/toggle-redshift.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#  ╺┳╸┏━┓┏━╸┏━╸╻  ┏━╸   ┏━┓┏━╸╺┳┓┏━┓╻ ╻╻┏━╸╺┳╸
+#   ┃ ┃ ┃┃╺┓┃╺┓┃  ┣╸    ┣┳┛┣╸  ┃┃┗━┓┣━┫┃┣╸  ┃
+#   ╹ ┗━┛┗━┛┗━┛┗━╸┗━╸   ╹┗╸┗━╸╺┻┛┗━┛╹ ╹╹╹   ╹
+
+if [[ $(pidof redshift | wc -w) == 0 ]]; then
+  redshift -m randr
+else
+  pkill redshift
+fi
+
+pkill -RTMIN+2 i3blocks
