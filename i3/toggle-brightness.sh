f9e653738370acdf97a5924f67264d9288b25437
Oskar Grunning <oskargrunning@gmail.com>
2021-04-02 10:34:25 +0200

    Change brightness scripts

--- i3/toggle-brightness.sh
+++ i3/toggle-brightness.sh
@@ -6,10 +6,14 @@
 
 current_brightness=$(light | cut -d '.' -f 1)
 
-if [[ current_brightness -lt 20 ]]; then
-  light -S 40 && pkill -RTMIN+10 i3blocks
-elif [[ current_brightness -lt 60 ]]; then
-  light -S 80 && pkill -RTMIN+10 i3blocks
+if [[ current_brightness -lt 30 ]]; then
+  light -S 30 && pkill -RTMIN+10 i3blocks
+elif [[ current_brightness -lt 50 ]]; then
+  light -S 50 && pkill -RTMIN+10 i3blocks
+elif [[ current_brightness -lt 70 ]]; then
+  light -S 70 && pkill -RTMIN+10 i3blocks
+elif [[ current_brightness -lt 90 ]]; then
+  light -S 90 && pkill -RTMIN+10 i3blocks
 else
   light -S 10 && pkill -RTMIN+10 i3blocks
 fi
