6f6f7440cfb3e42835e57336601f69389f9c9ecc
Oskar Grunning <oskargrunning@gmail.com>
2021-05-26 08:57:43 +0200

    chore: launch Rooter on note open

--- i3/menu-notes.sh
+++ i3/menu-notes.sh
@@ -10,5 +10,5 @@ notes="$(command fd --extension md --max-depth 1 . "$NOTES" | sed -r -e 's/\.md/
 choice="$(echo "$notes" | rofi -kb-accept-entry "Return" -dmenu -theme-str 'inputbar { children: [prompt, entry]; }' -p 'note: ')"
 
 if [[ -n "$choice" && -f "$NOTES/$choice.md" ]]; then
-  term nvim "$NOTES/$choice.md"
+  term nvim "$NOTES/$choice.md" +'Rooter'
 fi
