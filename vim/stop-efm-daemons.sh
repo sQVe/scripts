3f60d40c02b7964024ca2cee8d762731079261b7
Oskar Grunning <oskargrunning@gmail.com>
2021-09-10 14:55:34 +0200

    chore: add stop efm daemons scripts

---
+++ vim/stop-efm-daemons.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#  ┏━┓╺┳╸┏━┓┏━┓   ┏━╸┏━╸┏┳┓   ╺┳┓┏━┓┏━╸┏┳┓┏━┓┏┓╻┏━┓
+#  ┗━┓ ┃ ┃ ┃┣━┛   ┣╸ ┣╸ ┃┃┃    ┃┃┣━┫┣╸ ┃┃┃┃ ┃┃┗┫┗━┓
+#  ┗━┛ ╹ ┗━┛╹     ┗━╸╹  ╹ ╹   ╺┻┛╹ ╹┗━╸╹ ╹┗━┛╹ ╹┗━┛
+
+is_efm_running() {
+  pgrep efm-langserver &> /dev/null
+}
+
+stop_daemons() {
+  pkill eslint_d
+  pkill prettier_d_slim
+}
+
+# Exit if efm is running.
+if is_efm_running; then
+  exit 0
+fi
+
+sleep 1m
+! is_efm_running && stop_daemons
