2d4de80676f52267a536e916a3c3121c91f2c55a
Oskar Grunning <oskargrunning@gmail.com>
2022-01-24 16:27:32 +0100

    chore: tweak nvim and scrollback scripts

--- kitty/neovim-scrollback.sh
+++ kitty/neovim-scrollback.sh
@@ -6,15 +6,6 @@
 
 set -eu
 
-if [ "$#" -eq 3 ]; then
-  INPUT_LINE_NUMBER=${1:-0}
-  CURSOR_LINE=${2:-1}
-  CURSOR_COLUMN=${3:-1}
-  AUTOCMD_TERMCLOSE_CMD="call cursor(max([0,${INPUT_LINE_NUMBER}-1])+${CURSOR_LINE}, ${CURSOR_COLUMN})"
-else
-  AUTOCMD_TERMCLOSE_CMD="normal G"
-fi
-
 exec nvim \
   -u NONE \
   -c "noremap q ZZ" \
@@ -22,5 +13,5 @@ exec nvim \
   -c "noremap Ö :" \
   -c "set shell=bash scrollback=100000 termguicolors laststatus=0 clipboard+=unnamedplus" \
   -c "autocmd TermEnter * stopinsert" \
-  -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
+  -c "autocmd TermClose * normal Gkzb" \
   -c 'terminal sed </dev/fd/63 -e "s/'$'\x1b'']8;;file:[^\]*[\]//g" && sleep 0.01 && printf "'$'\x1b'']2;"' 63<&0 0< /dev/null
