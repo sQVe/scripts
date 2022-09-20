683b30736d1fdbbaa388d81be665fd27e4415e94
Oskar Grunning <oskargrunning@gmail.com>
2022-09-20 08:37:37 +0200

    chore: repair projects menu

--- i3/menu-projects.sh
+++ i3/menu-projects.sh
@@ -4,9 +4,9 @@
 #  ┃┃┃┣╸ ┃┗┫┃ ┃   ┣━┛┣┳┛┃ ┃  ┃┣╸ ┃   ┃ ┗━┓
 #  ╹ ╹┗━╸╹ ╹┗━┛   ╹  ╹┗╸┗━┛┗━┛┗━╸┗━╸ ╹ ┗━┛
 
-projects=$(fd --max-depth 3 --hidden --strip-cwd-prefix --exclude ".{local}" --type directory '^.git$' | sed -r 's/\/.git$//')
-choice="$(echo "$projects" | rofi -dmenu -p 'project')"
+projects=$(fd --max-depth 3 --hidden --strip-cwd-prefix --exclude ".{local}" --type directory '^.git$' | sed -r 's/\/.git\/$//')
+choice="$(echo "${projects}" | rofi -dmenu -p 'project')"
 
-if [[ -n "$choice" && -d "$HOME/$choice" ]]; then
-  term cd "$HOME/$choice"
+if [[ -n "${choice}" && -d "${HOME}/${choice}" ]]; then
+  term cd "${HOME}/${choice}"
 fi
