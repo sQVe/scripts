198e3eab2d263da5b23a0d6278b921c1d7b9dac4
Oskar Grunning <oskargrunning@gmail.com>
2022-10-06 11:59:03 +0200

    chore: fix linting issues of format script

--- format-marks.sh
+++ format-marks.sh
@@ -4,8 +4,8 @@
 #  ┣╸ ┃ ┃┣┳┛┃┃┃┣━┫ ┃    ┃┃┃┣━┫┣┳┛┣┻┓┗━┓
 #  ╹  ┗━┛╹┗╸╹ ╹╹ ╹ ╹    ╹ ╹╹ ╹╹┗╸╹ ╹┗━┛
 
-bookmarks="$DOTFILES/config/qutebrowser/bookmarks/urls"
-quickmarks="$DOTFILES/config/qutebrowser/quickmarks"
+bookmarks="${DOTFILES}/config/qutebrowser/bookmarks/urls"
+quickmarks="${DOTFILES}/config/qutebrowser/quickmarks"
 
-command nvim "$bookmarks" "+%sort /\v^.{-}\s+/ u" "+%EasyAlign \ " "+wq"
-command nvim "$quickmarks" "+%sort u" "+%EasyAlign \ " "+wq"
+command nvim "${bookmarks}" "+%sort /\v^.{-}\s+/ u" "+%EasyAlign \ " "+wq"
+command nvim "${quickmarks}" "+%sort u" "+%EasyAlign \ " "+wq"
