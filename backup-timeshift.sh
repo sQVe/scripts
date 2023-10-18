1668bd931dab284b61538baa1422e8dc012944a8
Oskar Grunning <oskargrunning@gmail.com>
2023-10-18 15:49:11 +0200

    fix: apply shellcheck fixes

--- backup-timeshift.sh
+++ backup-timeshift.sh
@@ -11,7 +11,7 @@ fi
 
 path="$1/systems/$(nmcli general hostname)"
 
-sudo mkdir -p "$path"
-sudo rsync -AHPXa --delete --delete-excluded /timeshift "$path"
+sudo mkdir -p "${path}"
+sudo rsync -AHPXa --delete --delete-excluded /timeshift "${path}"
 
-echo "Successfully created timeshift backup at: $path."
+echo "Successfully created timeshift backup at: ${path}."
