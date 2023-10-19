bca117547b907334b9f84b6e8250ecdeb3de0680
Oskar Grunning <oskargrunning@gmail.com>
2023-10-19 18:23:43 +0200

    chore: simplify mons handler
    The setprovideroutputsource isn't needed when using MUX switch.

--- mons-handler.sh
+++ mons-handler.sh
@@ -3,9 +3,4 @@
 # ┃┃┃┃ ┃┃┗┫┗━┓   ┣━┫┣━┫┃┗┫ ┃┃┃  ┣╸ ┣┳┛
 # ╹ ╹┗━┛╹ ╹┗━┛   ╹ ╹╹ ╹╹ ╹╺┻┛┗━╸┗━╸╹┗╸
 
-# Setup xorg for dGPU (nvidia) only mode.
-if [[ $(envycontrol --query) == "nvidia" ]]; then
-  xrandr --setprovideroutputsource modesetting NVIDIA-0
-fi
-
 xrandr --auto
