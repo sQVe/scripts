6eb3698e36cecf143f847706d1c9ddd27332ee6d
Oskar Grunning <oskargrunning@gmail.com>
2023-10-20 16:36:19 +0200

    Revert "chore: simplify mons handler"
    This reverts commit bca117547b907334b9f84b6e8250ecdeb3de0680.

--- mons-handler.sh
+++ mons-handler.sh
@@ -3,4 +3,9 @@
 # ┃┃┃┃ ┃┃┗┫┗━┓   ┣━┫┣━┫┃┗┫ ┃┃┃  ┣╸ ┣┳┛
 # ╹ ╹┗━┛╹ ╹┗━┛   ╹ ╹╹ ╹╹ ╹╺┻┛┗━╸┗━╸╹┗╸
 
+# Setup xorg for dGPU (nvidia) only mode.
+if [[ $(envycontrol --query) == "nvidia" ]]; then
+  xrandr --setprovideroutputsource modesetting NVIDIA-0
+fi
+
 xrandr --auto
