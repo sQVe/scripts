6c303ef98d680b78485a4ebe1cd06e6bb800132f
Oskar Grunning <oskargrunning@gmail.com>
2024-05-20 15:51:15 +0200

    chore: use rofimoji

--- menus/emoji.sh
+++ menus/emoji.sh
@@ -4,4 +4,4 @@
 #  ┃┃┃┣╸ ┃┗┫┃ ┃   ┣╸ ┃┃┃┃ ┃  ┃┃
 #  ╹ ╹┗━╸╹ ╹┗━┛   ┗━╸╹ ╹┗━┛┗━┛╹
 
-rofi -modi emoji -show emoji -emoji-format '{emoji} {name}'
+rofimoji --skin-tone neutral --max-recent 0 --selector-args="-theme /usr/lib/python3.12/site-packages/picker/contrib/grid.rasi -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f" --hidden-descriptions
