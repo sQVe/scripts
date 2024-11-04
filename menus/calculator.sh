bea50e51fa8bfafb833887e1b51b230ab79771e4
Oskar Grunning <oskargrunning@gmail.com>
2024-11-04 15:04:36 +0100

    chore: remove calculator menu

--- menus/calculator.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#  ┏┳┓┏━╸┏┓╻╻ ╻   ┏━╸┏━┓╻  ┏━╸╻ ╻╻  ┏━┓╺┳╸┏━┓┏━┓
-#  ┃┃┃┣╸ ┃┗┫┃ ┃   ┃  ┣━┫┃  ┃  ┃ ┃┃  ┣━┫ ┃ ┃ ┃┣┳┛
-#  ╹ ╹┗━╸╹ ╹┗━┛   ┗━╸╹ ╹┗━╸┗━╸┗━┛┗━╸╹ ╹ ╹ ┗━┛╹┗╸
-
-rofi -show calc -modi calc -no-show-match -no-sort -terse -calc-command "echo -n '{result}' | xsel -ib"
