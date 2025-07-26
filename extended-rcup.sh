02674b20d4045018b0ef449cd7e0dc4263af4400
Oskar Grunning <oskargrunning@gmail.com>
2025-07-26 12:56:58 +0200

    chore: minor script tweaks

--- extended-rcup.sh
+++ extended-rcup.sh
@@ -10,7 +10,7 @@ rcup "$@"
 
 # Only create custom symlinks when script has no arguments.
 if [[ $# -eq 0 ]]; then
-  ln -s "$DOTFILES/config/zsh/zimrc" "$XDG_CONFIG_HOME/zsh/.zimrc"
-  ln -s "$DOTFILES/config/zsh/zshenv" "$XDG_CONFIG_HOME/zsh/.zshenv"
-  ln -s "$DOTFILES/config/zsh/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
+  ln -s "${DOTFILES}/config/zsh/zimrc" "${XDG_CONFIG_HOME}/zsh/.zimrc"
+  ln -s "${DOTFILES}/config/zsh/zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
+  ln -s "${DOTFILES}/config/zsh/zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
 fi
