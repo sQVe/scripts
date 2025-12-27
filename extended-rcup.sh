1c293b34bb8f4531cfe1bd415daf89904d4558a8
Oskar Grunning <oskargrunning@gmail.com>
2025-12-27 20:22:57 +0100

    fix: improve script robustness
    - extended-rcup: use ln -sf to allow re-running
    - playground: fix path quoting

--- extended-rcup.sh
+++ extended-rcup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 #  ┏━╸╻ ╻╺┳╸┏━╸┏┓╻╺┳┓┏━╸╺┳┓   ┏━┓┏━╸╻ ╻┏━┓
 #  ┣╸ ┏╋┛ ┃ ┣╸ ┃┗┫ ┃┃┣╸  ┃┃   ┣┳┛┃  ┃ ┃┣━┛
 #  ┗━╸╹ ╹ ╹ ┗━╸╹ ╹╺┻┛┗━╸╺┻┛   ╹┗╸┗━╸┗━┛╹
@@ -10,7 +12,7 @@ rcup "$@"
 
 # Only create custom symlinks when script has no arguments.
 if [[ $# -eq 0 ]]; then
-  ln -s "${DOTFILES}/config/zsh/zimrc" "${XDG_CONFIG_HOME}/zsh/.zimrc"
-  ln -s "${DOTFILES}/config/zsh/zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
-  ln -s "${DOTFILES}/config/zsh/zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
+  ln -sf "${DOTFILES}/config/zsh/zimrc" "${XDG_CONFIG_HOME}/zsh/.zimrc"
+  ln -sf "${DOTFILES}/config/zsh/zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
+  ln -sf "${DOTFILES}/config/zsh/zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
 fi
--- playground.sh
+++ playground.sh
@@ -8,4 +8,4 @@
 set -euo pipefail
 
 term --class "playground" \
-  "builtin cd ${CODE}/personal/playground && \nvim +vsplit +\"terminal npm run start\" +'wincmd h' +'norm G$' \"src/index.ts\""
+  "builtin cd \"${CODE}/personal/playground\" && nvim +vsplit +\"terminal npm run start\" +'wincmd h' +'norm G\$' \"src/index.ts\""
