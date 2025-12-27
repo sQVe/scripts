cfc6f3256cc4870446612107865e3e7114e872c5
Oskar Grunning <oskargrunning@gmail.com>
2025-12-27 23:06:25 +0100

    fix(emoji): resolve glob pattern in theme path
    The glob pattern was passed as a literal string to rofi, causing it
    to fail when the python version changed.

--- menus/emoji.sh
+++ menus/emoji.sh
@@ -6,4 +6,6 @@ set -euo pipefail
 #  ┃┃┃┣╸ ┃┗┫┃ ┃   ┣╸ ┃┃┃┃ ┃  ┃┃
 #  ╹ ╹┗━╸╹ ╹┗━┛   ┗━╸╹ ╹┗━┛┗━┛╹
 
-rofimoji --action clipboard --skin-tone neutral --max-recent 0 --selector-args="-theme /usr/lib/python3.*/site-packages/picker/contrib/grid.rasi -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f" --hidden-descriptions
+theme="$(echo /usr/lib/python3.*/site-packages/picker/contrib/grid.rasi)"
+
+rofimoji --action clipboard --skin-tone neutral --max-recent 0 --selector-args="-theme ${theme} -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f" --hidden-descriptions
