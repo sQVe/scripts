1c5be94a7b0936bd147dc8bd12b8b9162be2ea90
Oskar Grunning <oskargrunning@gmail.com>
2025-12-27 23:06:31 +0100

    fix(passwords): make filter argument optional
    Default to matching all entries when no filter is provided.

--- menus/passwords.sh
+++ menus/passwords.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 #  ╹ ╹┗━╸╹ ╹┗━┛   ╹  ╹ ╹┗━┛┗━┛┗┻┛┗━┛╹┗╸╺┻┛┗━┛
 
 passwords=$(fd --extension gpg . "${PASSWORD_STORE_DIR}" | sed "s#${PASSWORD_STORE_DIR}/##" | sed 's/\.gpg$//')
-choice="$(echo "${passwords}" | rg "$1" | rofi -dmenu -p 'password')"
+choice="$(echo "${passwords}" | rg "${1:-.}" | rofi -dmenu -p 'password')"
 
 if [[ -n "${choice}" ]]; then
   if pass show --clip "${choice}"; then
