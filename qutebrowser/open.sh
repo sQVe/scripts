6dc18595da37b303440df54fab95659b77fbf244
Oskar Grunning <oskargrunning@gmail.com>
2026-01-01 21:29:28 +0100

    feat(qutebrowser): add chrome option to open menu

--- qutebrowser/open.sh
+++ qutebrowser/open.sh
@@ -7,6 +7,7 @@
 set -euo pipefail
 
 open_options=(
+  "chrome"
   "mimeo"
   "mpv"
   "xh"
@@ -15,6 +16,9 @@ open_choice=$(printf '%s\n' "${open_options[@]}" | rofi -dmenu -p 'open')
 
 if [[ -n "${open_choice}" ]]; then
   case "${open_choice}" in
+    "chrome")
+      google-chrome-stable "${*}" &
+      ;;
     "mimeo")
       mimeo -q "${*}"
       ;;
